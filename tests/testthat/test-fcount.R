5850daec92c81fd177c615966a7727532f87c565
guoyunlong <guoyunlong10@gmail.com>
2019-03-23 22:58:51 +0800

    add doc and test for fcount

---
+++ tests/testthat/test-fcount.R
@@ -0,0 +1,9 @@
+context("Sort a factor")
+
+test_that("fcount sort a factor", {
+  x <- factor(c("a", "b", "a", "c", "c"))
+  z <- tibble(f = factor(c("a", "c", "b")),
+              n = as.integer(c(2, 2, 1)))
+  expect_identical(fcount(x), z)
+})
+
